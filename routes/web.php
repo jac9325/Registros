e0d616ba9400b83c81fd21e5cacb7bbc429ad2a7
jaime amilcar <jac,s9325@gmail.com>
2021-10-19 06:59:42 -0500

    agregando todo el crud a mi pryecto

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('registros',[RegistroController::class,'index'])->name('registros.index');
+Route::get('registros/create',[RegistroController::class,'create'])->name('registros.create');
+Route::get('registros/{id}',[RegistroController::class,'show'])->name('registros.show');
+
+Route::post('registros',[RegistroController::class,'store'])->name('registros.store');
+Route::get('registros/{id}/edit', [RegistroController::class,'edit'])->name('registros.edit');
+Route::put('registros/{registro}',[RegistroController::class,'update'])->name('registros.update');
